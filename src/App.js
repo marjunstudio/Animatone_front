ef551b453353e7eeb9039e6a329072cf12474d48
marjunstudio <uebujie@gmail.com>
2024-03-18 22:04:16 +0900

    fix:不要なクラスを削除

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/organisms/Footer/Footer';
 
 function App() {
   return (
-    <div className="App flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow bg-slate-300">
         <div>
